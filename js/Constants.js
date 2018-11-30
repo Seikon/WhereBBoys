0c2ac019a77e85bfc1c2ab8ff5f77d28e652dd8c
Alejandro <alejandro.fernandez@hec.de>
2018-11-30 14:56:25 +0100

    UI integrated and wizard almos completed

--- js/Constants.js
+++ js/Constants.js
@@ -1,1 +1,14 @@
 var SERVER_URL = "http://localhost:3000";
+
+var ID_FIRST_STEP = "stepOneColumn";
+var ID_SECOND_STEP = "stepTwoColumn";
+var ID_THIRD_STEP = "stepThreeColumn";
+
+var ID_OPTION_SEARCH = "optIdSearch";
+var ID_OPTION_ADD = "optIdAdd";
+
+var INTERACTION_MODE = {
+
+    SEARCH : 0,
+    ADD : 1
+}
